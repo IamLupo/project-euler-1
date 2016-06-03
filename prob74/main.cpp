bb6155ac7f6b8ed9afc876883e10edf78366cc0e
Hugo Cartwright <webossfr@gmail.com>
2016-06-03 21:26:18 +0200

    Update main.cpp

--- prob74/main.cpp
+++ prob74/main.cpp
@@ -4,11 +4,19 @@
 
 using namespace std;
 
+//Factorial function
 long long factorial(long long a){
 	long long b = 1;
 	for(long long i = 1; i < a; i++){
 		b *= i+1;
 	}
+}
+
+//Factorials for digits 0-9
+vector<long long> digitFactorials;
+
+for(long long i = 0; i < 10; i++){
+	digitFactorials.push_back(factorial(i));
 }
 
 //return the array of digits that a is made of
@@ -21,62 +29,77 @@ void digits(long long a, &vector<long long> digitsArray){
 	digitsArray.push_back(digit);
 }
 
-//return the sum of an array
-long long sum(vector<long long> a){
-	long long b = 0;
-	for(long long i = 0; i < a.size(); i++){
-		b += a[i];
-	}
-	return b;
-}
-
-//return array of numbers of a non-repeating chain of digit factorial sums with starting number in argument
-vector <long long> digitFactorialSumChain(long long a){
+//for starting number a in argument, return the array of numbers of a non-repeating chain of digit factorial sums with 
+vector <long long> digitFactorialSumChainCount(long long a, vector<vector<long long>> &l, long long limit){
+	//digits in a
 	vector<long long> digitsArray = [];
+	
+	//factorial of each digit in a
 	vector<long long> factorialArray = [];
+	
+	//array of summed factorials to be returned
 	vector<long long> chainArray = [];
 	
-	chainArray.push_back(a);
-	digits(a, digitsArray);
-	
-	for(long long i = 0; i < digitsArray.size(); i++){
-		factorialArray.push_back(factorial(digitsArray[i]));
-	}
-	
-	long long newValue = sum(factorialArray);
-	
-	if (find(digitFactorialSumChain.begin(), digitFactorialSumChain.end(), newValue) != digitFactorialSumChain.end()){
-		digitFactorialSumChain(newValue);
-	}
-}
-
-//Alternative of below
-long long countFactorialChains(long long l, long long a, long long b){
-	for(long long i = a; i < b; i++){
-		vector<long long> tmp = digitFactorialSumChain(i);
-	}
-}
-
-//Count non-repeating chains of exactly length l, within an interval [a:b]
-long long countFactorialChains(long long l, long long a, long long b){
-	long long count = 0;
-	
-	for(long long i = a; i <= b; i++){
-		if(digitFactorialSumChain(i).size() == l){
-			count++;
+	//if a in chainArray
+	if (find(chainArray.begin(), chainArray.end(), a) != chainArray.end()){
+		if (a == chainArray[0]){
+			for(long long i = 0; i < chainArray.size(); i++){
+				l[chainArray.size()].push_back(chainArray[i]);
+			}
+		}else{
+			get position index p of a in chainArray
+			for (long long i = p ; i < chainArray.size(); i++){
+				push_back chainArray[i] to l at index j == chainArray.size() - p
+			}
+			for (long long i = 0; i < p; i++){
+				push_back chainArray[i] to l at index j == chainArray.size() - i
+			}	
+		}
+	}else{
+		//if a in l
+		for (int i = 0; i < l.size(); i++){
+			auto it = find(l[i].begin(), l[i].end(), a);
+			if (it != l[i].end()){
+				
+				//all the numbers that do not yet exist in l can be added to l at the same index as value a in l
+				//cout << "Number " << a << " found at l[" << i << "][" << distance(l[i].begin(), it) << "].";
+				for(long long j = 0; j < chainArray.size(); j++){
+					if(chainArray[j] not in l){
+						l[i].push_back(chainArray[j]);
+					}
+				}
+				
+			}else{
+				//add a to the array of summed factorials
+				chainArray.push_back(a);
+				
+				//get the digits of a
+				digits(a, digitsArray);
+				
+				//get the next factorial sum
+				long long tmp = 0;
+				for(long long i = 0; i < digitsArray.size(); i++){
+					tmp += digitFactorials[digitsArray[i]];
+				}
+				digitFactorialSumChainCount(tmp, l, limit);
+			}
 		}
 	}
-	
-	return count;
 }
 
 int main(){
-	vector<vector<long long>> lengths;
-	long long length = 60;
-	long long a = 1;
-	long long b = 999999;
-
-	cout << "Result for length " << length << " is: " << countFactorialChains(length, a, b) << endl;
-
+	
+	long long limit = 60;
+	long long max = 1000000;
+	vector<vector<long long>> l;
+	
+	for(long long i = 1; i < max; i++){
+		if(i not in l){
+			digitFactorialSumChainCount(i, l, limit);
+		}
+	}
+	
+	cout << l[limit].size() << endl;
+	
 	return 0;
 }
