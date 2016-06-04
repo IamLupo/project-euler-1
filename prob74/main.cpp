9b53d38b15522ca8500a2b382a17a8e355c2081a
root <root@vps268149.ovh.net>
2016-06-04 18:37:51 +0200

    clean up dysfunctional problems

--- prob74/main.cpp
+++ prob74/main.cpp
@@ -17,100 +17,40 @@ void digits(long long a, vector<long long> &digitsArray){
 	digitsArray.push_back(digit);
 }
 
-//for starting number a in argument, return the array of numbers of a non-repeating chain of digit factorial sums with 
-void digitFactorialSumChainCount(long long a, vector<vector<long long>> &l, long long limit){
-	//digits in a
-	vector<long long> digitsArray;
-	
-	//factorial of each digit in a
-	vector<long long> factorialArray;
-	
-	//array of summed factorials to be returned
-	vector<long long> chainArray;
-	
-	//if a in chainArray
-	if (find(chainArray.begin(), chainArray.end(), a) != chainArray.end()){
-		if (a == chainArray[0]){
-			for(long long i = 0; i < chainArray.size(); i++){
-				l[chainArray.size()].push_back(chainArray[i]);
-			}
-		}else{
-			//get position index p of a in chainArray
-			for (int i = 0; i < l.size(); i++){
-				auto it = find(l[i].begin(), l[i].end(), a);
-				if (it != l[i].end()){
-					long long p = i;
-					
-					for (long long j = p ; j < chainArray.size(); j++){
-					l[(chainArray.size() - p)].push_back(chainArray[j]);
-					}
-					
-					for (long long j = 0; j < p; j++){
-						l[(chainArray.size() - j)].push_back(chainArray[j]);
-					}
-					
-				}
-			
-		}
-	}else{
-		//if a in l
-		for (int i = 0; i < l.size(); i++){
-			auto it = find(l[i].begin(), l[i].end(), a);
-			if (it != l[i].end()){
-				
-				//all the numbers that do not yet exist in l can be added to l at the same index as value a in l
-				//cout << "Number " << a << " found at l[" << i << "][" << distance(l[i].begin(), it) << "].";
-				for(long long j = 0; j < chainArray.size(); j++){
-					
-					for (int j = 0; j < l.size(); j++){
-						auto it = find(l[j].begin(), l[j].end(), i);
-						if (it != l[j].end()){
-							
-						}else{
-							//if(chainArray[j] not in l){
-							l[i].push_back(chainArray[j]);
-						}
-					}
-				}
-				
-			}else{
-				//add a to the array of summed factorials
-				chainArray.push_back(a);
-				
-				//get the digits of a
-				digits(a, digitsArray);
-				
-				//get the next factorial sum
-				long long tmp = 0;
-				for(long long i = 0; i < digitsArray.size(); i++){
-					tmp += digitFactorials[digitsArray[i]];
-				}
-				digitFactorialSumChainCount(tmp, l, limit);
-			}
-		}
-	}
+void chain(long long a, vector<long long> memo,int &count){
+    vector<long long> da;
+    long long result = 0;
+    digits(a, da);
+    //cout << a << endl;
+    for(long long i = 0; i < da.size(); i++){
+        result += digitFactorials[da[i]];
+    }
+    if(!(memo.empty()) && find(memo.begin(), memo.end(), result) != memo.end()){
+        if(memo.size() == 59){
+            count++;
+            for(int j = 0; j < memo.size(); j++){
+                cout << memo[j] << ", ";
+            }
+            cout << endl;
+        }
+    }else if(memo.size() > 60){
+        //
+    }else{
+        memo.push_back(result);
+        chain(result, memo, count);
+    }
 }
 
 int main(){
 	
 	long long limit = 60;
-	long long max = 1000000;
-	vector<vector<long long>> l;
-	
-	for(long long i = 1; i < max; i++){
-		
-		for (int j = 0; j < l.size(); j++){
-			auto it = find(l[j].begin(), l[j].end(), i);
-			if (it != l[j].end()){
-				
-			}else{
-				//if i not in l
-				digitFactorialSumChainCount(i, l, limit);
-			}
-		}
+	long long max = 1000000; 
+        //vector<long long> memo;
+        //long long hi = chain(max, memo);
+        int count = 0;
+        for(long long i = 1; i < max; i++){
+            vector<long long> memo;
+            chain(i, memo, count);
 	}
-	
-	cout << l[limit].size() << endl;
-	
 	return 0;
 }
