e53d94f3a69138674386d6c0c6d8e77499e4dba5
Hugo Cartwright <webossfr@gmail.com>
2016-07-02 20:33:41 +0200

    Update prob9.cpp

--- prob9/prob9.cpp
+++ prob9/prob9.cpp
@@ -2,34 +2,27 @@
 #include<cmath>
 using namespace std;
 
-bool isRoundNumber(float a){
-		if (ceilf(a) == a){
-				return true;
-		}
-		return false;
-}
-
 bool isPythagoreanTriplet(int a, int b, int c){
-		if(pow(a, 2) + pow(b, 2) == pow(c, 2)){
-				return true;
-		}
-return false;
+	if(pow(a, 2) + pow(b, 2) == pow(c, 2)){
+			return true;
+	}
+	return false;
 }
 
 int main(){
 
-		int n = 1000;
+	int n = 1000;
 
-		for(int a = 1; a <= n; a++){
-				for(int b = a + 1; b <= n - a; b++){
-						for(int c = b + 1; c <= n -b -a; c++){
-								if((a + b + c == n) && isPythagoreanTriplet(a, b, c)){
-										cout << a << ": " <<  b << ": " << c << endl;
-										cout << a * b * c << endl;
-								}
-						}
+	for(int a = 1; a <= n; a++){
+		for(int b = a + 1; b <= n - a; b++){
+			for(int c = b + 1; c <= n - b - a; c++){
+				if((a + b + c == n) && isPythagoreanTriplet(a, b, c)){
+					cout << a << ": " <<  b << ": " << c << endl;
+					cout << a * b * c << endl;
 				}
+			}
 		}
+	}
 
-		return 0;
+	return 0;
 }
