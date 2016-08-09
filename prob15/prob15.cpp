d85cf4f9a5ee8f42b2473bc53af7afacbe62d259
IamLupo <help0001@gmail.com>
2016-08-09 10:18:26 +0200

    Update prob15.cpp
    made some existing C++ functionality change. Makes the code shorter ;)

--- prob15/prob15.cpp
+++ prob15/prob15.cpp
@@ -4,37 +4,27 @@
 using namespace std;
 
 int main(){
-int max = 20;
-
-//filling variable array of size max with 1's
-vector<long long int> array;
-for(int i = 0; i < max; i++){
-	array.push_back(1);
-}
-
-//computing solution and storing in array
-for(int i = 0; i < max - 1; i++){
-	long long int a = 0;
-	long long int b = 1;
-	for(int j = 0; j < max - 1; j++){
-		if(i == 0){
-			a = 1;
-		}else{
-			a = array[j + 1];
+	int max = 20;
+	
+	//filling variable array of size max with 1's
+	vector<long long int> array(max, 1);
+	
+	//computing solution and storing in array
+	for(int i = 0; i < max - 1; i++){
+		long long int a = 0;
+		long long int b = 1;
+		for(int j = 0; j < max - 1; j++){
+			if(i == 0){
+				a = 1;
+			}else{
+				a = array[j + 1];
+			}
+			b += a;
+			array[j + 1] = b;
 		}
-		b += a;
-		array[j + 1] = b;
 	}
-}
-
-//sum of array variable
-long long int total = 0;
-for(int i = 0; i < array.size(); i++){
-	total += array[i];
-}
-
-cout << 2 * total << endl;
-
-return 0;
-
+	
+	cout << 2 * accumulate(array.begin(), array.end(), 0) << endl;
+	
+	return 0;
 }
